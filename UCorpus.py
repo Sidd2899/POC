d294203aec056212fe69851d5261c3f1e3b9faed
siddhant <siddhant2899@gmail.com>
2022-09-12 03:01:09 +0550

    abc

---
+++ UCorpus.py
@@ -0,0 +1,38 @@
+from dvc.repo import Repo
+import git
+
+class UCorpus:
+    # initialize local repository
+    def init(self,localRepo):
+        self.d = Repo.init(localRepo,no_scm=True,force=True)
+        self.g = git.Repo.init(localRepo)
+    #adding files .dvc and to put current working files into the current index ) for dvc command and rest for git
+    def add(self,folder,filetype = 0):
+        defaultfile = '--all'
+        if filetype == 0:
+            Repo.add(self.d,folder)
+        else: self.g.git.add(defaultfile)
+    
+    #Clone an existing repository into a new directory,
+    def clone(localRepo,RepoAddress):
+        git.Git(localRepo).clone(RepoAddress)
+    def fetch(self):
+        self.g.remote().fetch()
+
+    def remote(self,remoterepolocation,gitrepoloc):
+        with self.d.config.edit() as conf:
+            conf["remote"]["remote_store"] = {"url": str(remoterepolocation)}
+        self.g.delete_remote('master')
+        self.g.create_remote('master',str(gitrepoloc))
+    def push(self):
+        Repo.push(self.d,remote='remote_store')
+        print(self.g.remotes.master.push())
+        
+    
+    def commit(self,localRepo,message = ''):
+        self.d.commit(target=localRepo,force=True)
+        self.g.index.commit(message)
+
+
+
+    
---
+++ poc.py
@@ -0,0 +1,18 @@
+from re import S
+from UCorpus import UCorpus
+
+s = UCorpus
+
+s.init(s,'/home/siddhant/Audiofiles/POC')
+
+s.add(s,'/home/siddhant/Audiofiles/POC/2BqVo8kVB2Skwgyb',0)
+s.add(s,'https://github.com/Sidd2899/POC.git',1)
+s.commit(s,'/home/siddhant/Audiofiles/POC/2BqVo8kVB2Skwgyb.dvc','abc')
+s.remote(s,'s3://uniphoredataops/data/','https://github.com/Sidd2899/POC.git')
+s.fetch(s)
+
+s.push(s)
+
+
+
+
